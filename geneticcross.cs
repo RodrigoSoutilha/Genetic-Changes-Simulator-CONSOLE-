c059f24b3ff50774e8665cd68349125187641ce0
Rodrigo Soutilha <44254329+RodrigoSoutilha@users.noreply.github.com>
2019-05-17 20:46:17 -0300

    Update geneticcross.cs

--- geneticcross.cs
+++ geneticcross.cs
@@ -6,8 +6,12 @@ public class Program
     {
         Individuals setNewSimulation = new Individuals(0);
         setNewSimulation.setSeeds();
+
     }
+
 }
+
+
 public class Individuals
 {
     public int[] geneticConfiguration { get; set; }
@@ -19,15 +23,17 @@ public class Individuals
         geneticConfiguration = new int[numberOfChosenGenes];
         chosenGenes = new int[numberOfChosenGenes];
     }
+
     public int setSeeds() ///////////////////////////////////AQUI////////////////////////////////////////
     {
         int i;
         int j;
         int k;
-	int h;
-	int g;
-	int choose_gene;
-	Random randomNumber = new Random();
+        int h;
+        int g;
+        int f;
+        int choose_gene;
+        Random randomNumber = new Random();
         Console.WriteLine("Number of seeds: "); // Number of generations
         int numberOfSeeds = int.Parse(Console.ReadLine()); ;
         Console.Clear();
@@ -37,37 +43,41 @@ public class Individuals
         Console.Clear();
         this.numberOfChosenGenes = getnumberOfChosenGenes;
 
-        for (i = 0; i < numberOfSeeds; i++)
+        for (i = 0; i < numberOfSeeds; i++) //FUNCIONANDO!
         {
-        	individualsArray[i] = new Individuals(getnumberOfChosenGenes);
-	}
-        for (h = 0; h < numberOfChosenGenes;h++) //get genes the user will simulate and store in the individuals inside individuals array
-	{
-		Console.WriteLine("Choose genes");
-		choose_gene = int.Parse(Console.ReadLine());
-		for(g = 0;g<numberOfSeeds;g++)
-		{
-			individualsArray[g].chosenGenes[h] = choose_gene;
-		}
-	}		
-	Console.WriteLine("How do you want the seeds's genetics?");
+            individualsArray[i] = new Individuals(getnumberOfChosenGenes);
+        }
+        Console.WriteLine("Choose genes");
+        Console.WriteLine("[1] [2] [3] [4] [5] [6] [7] [8] [9] [10]");
+        choose_gene = int.Parse(Console.ReadLine());
+        for (g = 0; g < numberOfChosenGenes; g++)  //AJEITAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAR PARA ADICIONAR OS GENES ESCOLHIDOS NA ARRAY chosenGenes[] DE CADA SEED!
+        {
+            for (f = 0; f < numberOfSeeds; f++)
+            {
+                
+                individualsArray[f].chosenGenes[g] = choose_gene;
+            }
+        }
+        Console.WriteLine("How do you want the seeds's genetics?");
         Console.WriteLine("[1] Random");
         Console.WriteLine("[2] Custom");
         int choose = int.Parse(Console.ReadLine()); //aqui também
         if (choose == 1)
         {
-		for(k=0;k<numberOfChosenGenes;k++)
-	{
-          	individualsArray[k].geneticConfiguration[k] = randomNumber.Next(0,4);
-		Console.WriteLine(individualsArray[k].geneticConfiguration[k]);
-	}  
-}
-        else 
-	{
-	//custom
-	}
-        Console.WriteLine("skrr skrr skrr"); //cola num compilador online pra vc testar
+            for (k = 0; k < numberOfChosenGenes; k++) //AJEITAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAR PARA GERAR NUMEROS ALEATORIOS ENTRE 0 E 3 E COLOCAR NA ARRAY geneticConfiguration[] DE CADA INDIVÍDIUO
+            {
+            individualsArray[k].geneticConfiguration[k] = randomNumber.Next(0, 4);
+            Console.WriteLine(individualsArray[k].geneticConfiguration[k]);
+            }
+        }
+        else
+        {
+        //custom
+        }
+        Console.WriteLine("skrr skrr skrr"); 
         Console.ReadLine();
         return 0;
     }
+
+
 }
