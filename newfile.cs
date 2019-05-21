914c7f82e7948788b2f86289c7b63789eea27632
Rodrigo Soutilha <44254329+RodrigoSoutilha@users.noreply.github.com>
2019-05-21 07:26:14 -0300

    Create newfile.cs

---
+++ newfile.cs
@@ -0,0 +1,145 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Settings settings = new Settings();
+        Individuals individual = new Individuals();
+        settings.setNumberOfSeeds();
+        settings.setSeedsNumberOfGenes();
+        settings.setChosenGenes(settings.numberOfSeedsGenes);
+        settings.setGenerations();
+        Individuals[] individualsArray = new Individuals[9999999];
+        settings.buildSeeds(settings.numberOfSeeds, settings.numberOfSeedsGenes, settings.chosenGenes, individualsArray);
+        Console.WriteLine(settings.numberOfChosenGenes);
+        Console.WriteLine("end");
+        Console.ReadLine();
+    }
+
+
+}
+
+
+public class Individuals
+{
+    public int[] geneticConfiguration { get; set; }
+    public int[] chosenGenes { get; set; }
+    public int numberOfChosenGenes;
+    public int numberOfSeeds;
+    public int numberOfSeedsGenes;
+}
+
+
+public class Settings
+{
+    public int numberOfChosenGenes;
+    public int[] chosenGenes = new int[900];
+    public int numberOfSeeds;
+    public int numberOfSeedsGenes;
+    private string choose;
+    public int generations;
+
+    public int setNumberOfSeeds()
+    {
+        Console.WriteLine("Number of seeds: ");
+        try
+        {
+            this.numberOfSeeds = int.Parse(Console.ReadLine());
+        }
+        catch
+        {
+            Console.Clear();
+            Console.WriteLine("enter a valid value");
+            this.setNumberOfSeeds();
+        }
+        this.confirmationScreen();
+        if (this.choose == "Y")
+        {
+
+        }
+        else if (this.choose == "N")
+        {
+            Console.Clear();
+            this.setNumberOfSeeds();
+        }
+        Console.Clear();
+        return this.numberOfSeeds;
+    }
+    public int setSeedsNumberOfGenes()
+    {
+        Console.WriteLine("Type the number of genes you wanna work with");
+        try
+        {
+            this.numberOfSeedsGenes = int.Parse(Console.ReadLine());
+        }
+        catch
+        {
+            Console.WriteLine("enter a valid value");
+            Console.Clear();
+            this.setSeedsNumberOfGenes();
+        }
+        confirmationScreen();
+        Console.Clear();
+        return this.numberOfSeedsGenes;
+    }
+
+    public string confirmationScreen()
+    {
+        Console.WriteLine("confirm? [Y/N]");
+        this.choose = Console.ReadLine();
+        if (this.choose == "Y" || this.choose == "y")
+        {
+            this.choose = "Y";
+        }
+
+        else if (this.choose == "N" || this.choose == "n")
+        {
+            this.choose = "N";
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("enter a valid value");
+            this.confirmationScreen();
+        }
+        return this.choose;
+    }
+
+    public int[] setChosenGenes(int numberOfSeedsGenes)
+    {
+        for (int i = 0; i < this.numberOfSeedsGenes; i++)
+        {
+            Console.WriteLine("Choose genes\n");
+            Console.WriteLine("[1] [2] [3] [4] [5] [6] [7] [8] [9] [10]\n");
+            Console.WriteLine(numberOfSeedsGenes - i + " more gene(s) to choose");
+            this.chosenGenes[i] = int.Parse(Console.ReadLine());
+            Console.Clear();
+        }
+        return this.chosenGenes;
+    }
+
+    public void buildSeeds(int numberOfSeeds, int numberOfSeedsGenes, int[] chosenGenes, Individuals[] individualsArray)
+    {
+        for (int i = 0; i < this.numberOfSeeds; i++)
+        {
+            individualsArray[i] = new Individuals();
+        }
+
+        for (int g = 0; g < this.numberOfSeedsGenes; g++)
+        {
+            for (int f = 0; f < this.numberOfSeeds; f++)
+            {
+                individualsArray[f].chosenGenes[g] = this.chosenGenes[g];
+            }
+        }
+    }
+
+    public int setGenerations()
+    {
+        Console.WriteLine("Enter the number of generations");
+        this.generations = int.Parse(Console.ReadLine());
+        return this.generations;
+    }
+
+}
