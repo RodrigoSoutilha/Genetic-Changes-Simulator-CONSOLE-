6e2739c17cdf1babc98c9a4fea19138fbba8d5f4
Rodrigo Soutilha <44254329+RodrigoSoutilha@users.noreply.github.com>
2019-06-07 09:41:43 -0300

    Update simulator.cpp

--- simulator.cpp
+++ simulator.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <conio.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -21,22 +22,101 @@ struct Settings
     string confirmationScreen()
     {
         cout<<"Confirm? [Y/N]";
+        cin>>choose;
+        if (choose == "Y")
+        {
+
+        }
+        else if(choose == "y")
+        {
+
+        }
+        else if (choose == "N")
+        {
+            system("CLS");
+            //return to calling function
+        }
+        else if (choose == "n")
+        {
+            system("CLS");
+            //return to calling function
+        }
+        else
+        {
+            system("CLS");
+            cout<<"You must enter a valid value..."<<endl;
+            confirmationScreen();
+        }
+
     }
 
     int setNumberOfSeeds()
     {
-        cout<<"Number of seeds: ";
+        cout<<"Number of seeds:  ";
         try
         {
-            cin>>choose;
+            cin>>this->numberOfSeeds;
         }
         catch(const std::exception& e)
         {
-            std::cerr << e.what() << '\n';
+            system("CLS");
+            cout<<"The number of seeds must be an integer..."<<endl;
+            this->setNumberOfSeeds();
         }
+        if (this->numberOfSeeds == 0)
+        {
+            system("CLS");
+            cout<<"The number of seeds cannot be zero..."<<endl;
+            this->setNumberOfSeeds();
+        }
+        else if (this->numberOfSeeds < 0)
+        {
+            system("CLS");
+            cout<<"The number of seeds cannot be minor than zero..."<<endl;
+            this->setNumberOfSeeds();
+        }
+        else
+        {
+        this->confirmationScreen();
+        }
+        return this->numberOfSeeds;
+    }
 
+    int setNumberOfSeedsGenes()
+    {
+        cout<<"Number of genes in the seeds genome: "<<endl;
+        try
+        {
+            cin>>this->numberOfSeedsGenes;
+        }
+        catch(const std::exception& e)
+        {
+            system("CLS");
+            cout<<"The number of genes must be an integer..."<<endl;
+            this->setNumberOfSeedsGenes();
+        }
+        
+        if(numberOfSeeds == 0)
+        {
+            system("CLS");
+            cout<<"The genome cannot have zero genes"<<endl;
+            this->setNumberOfSeedsGenes();
+        }
+        else if(numberOfSeeds < 0)
+        {
+            system("CLS");
+            cout<<"The genome cannot be minor than zero"<<endl;
+            this->setNumberOfSeedsGenes();
+        }
+        else
+        {
+            confirmationScreen();
+        }
+        return this->numberOfSeeds;
     }
 };
+
+
 
 struct Individuals 
 {
@@ -67,5 +147,13 @@ struct Simulation
 
 int main() 
 {
-
+    system("CLS");
+    system("color 1F");
+    //Thread displaying the seeds info being chosen by the user
+    Settings settings;
+    Individuals individuals;
+    Simulation simulation;
+    settings.setNumberOfSeeds();
+    settings.setNumberOfSeedsGenes();
+    settings.setChosenGenes(settings.numberOfChosenGenes);
 }
